9e1859cfc9f4d2a87477559d08581196b1903ac0
Eric Ihli <ericihli@gmail.com>
2016-04-02 00:24:51 -0700

    Add a mock data file

---
+++ data/todo-items.js
@@ -0,0 +1,26 @@
+[
+  {
+    "id": "1",
+    "text": "go to the store"
+  },
+  {
+    "id": "2",
+    "text": "buy some coffee"
+  },
+  {
+    "id": "3",
+    "text": "grind coffee"
+  },
+  {
+    "id": "4",
+    "text": "breW coffee"
+  },
+  {
+    "id": "5",
+    "text": "drink coffee"
+  },
+  {
+    "id": "6",
+    "text": "COOOODDDEEEEEEEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!111!1"
+  }
+]
